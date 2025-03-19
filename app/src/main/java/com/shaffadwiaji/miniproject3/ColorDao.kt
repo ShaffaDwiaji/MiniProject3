cbe44656ac2338bb51b0c762c84ff8d4a953c7f5
ShaffaDwiaji <amagiri7141@google.com>
2025-03-20 00:10:08 +0700

    Update - Menambahkan interface Color Dao dengan anotasi Dao

---
+++ app/src/main/java/com/shaffadwiaji/miniproject3/ColorDao.kt
@@ -0,0 +1,23 @@
+package com.shaffadwiaji.miniproject3
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+
+@Dao
+interface ColorDao {
+    @Query("SELECT * FROM Color")
+    fun getAll(): List<Color>
+
+    @Insert
+    fun insert(vararg colors: Color)
+
+    @Update
+    fun update(colors: Color)
+
+    @Delete
+    fun delete(colors: Color)
+
+}
