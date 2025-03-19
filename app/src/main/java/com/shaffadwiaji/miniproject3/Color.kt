74f5ac71a9b9ed114b9cb2c73a61e9f9589ae247
ShaffaDwiaji <amagiri7141@google.com>
2025-03-20 00:07:45 +0700

    Update - Menambahkan data class Color dan menambahkan anotasi Entity, PrimaryKey, dan ColumnInfo

---
+++ app/src/main/java/com/shaffadwiaji/miniproject3/Color.kt
@@ -0,0 +1,13 @@
+package com.shaffadwiaji.miniproject3
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+
+@Entity
+data class Color(
+    @PrimaryKey(autoGenerate = true) val _id: Int,
+    @ColumnInfo(name = "hex_color") val hex: String,
+    val name:String
+)
